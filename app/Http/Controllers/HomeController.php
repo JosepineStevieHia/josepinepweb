0b72f5369b817b1f4d13d45c283446f41eacffb5
JosepineStevieHia <steviehia06@gmail.com>
2025-03-09 15:22:53 +0700

    Update Readme , dan penambahan file baru test44.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'nama' => 'Josepine',
+            'pekerjaan' => 'Mahasiswa',
+        ];
+
+        return view('home')->with($data);
+
+        $nama = 'Raapi';
+        $pekerjaan = 'Mahasiswa';
+        return view('home', compact('nama', 'pekerjaan'));
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+}
